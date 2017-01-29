7703766affab16dd23f111efd5e04ccca56185c6
Mark Heckler <mark.heckler@gmail.com>
2017-01-29 14:29:35 -0600

    Updated to replace ClientRequest.GET, which was removed in the latest buildof Spring Web Reactive, with ClientRequest.method(HttpMethod.GET...) & add UriTemplate to build URI w/path variables

--- flux-flix-client/src/main/java/com/example/FluxFlixClientApplication.java
+++ flux-flix-client/src/main/java/com/example/FluxFlixClientApplication.java
@@ -5,45 +5,49 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriTemplate;
 
+import java.net.URI;
 import java.util.Date;
-
-import static org.springframework.web.reactive.function.client.ClientRequest.GET;
 
 @SpringBootApplication
 public class FluxFlixClientApplication {
 
-	@Bean
-	WebClient webClient() {
-		return WebClient.create(new ReactorClientHttpConnector());
-	}
+    @Bean
+    WebClient webClient() {
+        return WebClient.create(new ReactorClientHttpConnector());
+    }
 
-	@Bean
-	CommandLineRunner demo(WebClient client) {
-		return args ->
-				client.exchange(GET("http://localhost:8080/movies").build())
-						.flatMap(m -> m.bodyToFlux(Movie.class))
-						.filter(m -> m.getTitle().equalsIgnoreCase("flux gordon"))
-						.subscribe(m -> client.exchange(GET("http://localhost:8080/movies/{id}/streams", m.getId()).build())
-								.subscribe(cr -> cr.bodyToFlux(MovieStream.class).subscribe(System.out::println)));
+    @Bean
+    CommandLineRunner demo(WebClient client) {
+        return args ->
+                client.exchange(ClientRequest.method(HttpMethod.GET, new URI("http://localhost:8080/movies")).build())
+                        .flatMap(m -> m.bodyToFlux(Movie.class))
+                        .filter(m -> m.getTitle().equalsIgnoreCase("flux gordon"))
+                        .subscribe(m -> client.exchange(ClientRequest.method(HttpMethod.GET,
+                                new UriTemplate("http://localhost:8080/movies/{id}/streams")
+                                        .expand(m.getId())).build())
+                                .subscribe(cr -> cr.bodyToFlux(MovieStream.class)
+                                        .subscribe(System.out::println)));
+    }
 
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(FluxFlixClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FluxFlixClientApplication.class, args);
+    }
 }
 
 @Data
 class MovieStream {
-	private Date when;
-	private Movie movie;
-	private String user;
+    private Date when;
+    private Movie movie;
+    private String user;
 }
 
 @Data
 class Movie {
-	private String id, genre, title;
+    private String id, genre, title;
 }
