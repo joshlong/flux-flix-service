9d9644574205c59c3ecc575fb0993c3a586bcf40
Mark Heckler <mark.heckler@gmail.com>
2017-01-31 15:03:32 -0600

    Replaced explicit SSE creation with produces in GetMapping

--- flux-flix-service/src/main/java/com/example/FluxFlixServiceApplication.java
+++ flux-flix-service/src/main/java/com/example/FluxFlixServiceApplication.java
@@ -15,6 +15,7 @@ import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguratio
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,110 +35,111 @@ import java.util.stream.Stream;
 @SpringBootApplication(exclude = MongoDataAutoConfiguration.class)
 public class FluxFlixServiceApplication {
 
-	@Bean
-	CommandLineRunner demoData(MovieRepository movieRepository) {
-		return args -> {
-			movieRepository.deleteAll().block();
-			Flux<Movie> flux = Flux.fromStream(Stream.of("Y Tu Mono Tambien", "Flux Gordon", "Silence of the Lambdas", "FluxMan vs Superman",
-					"The Fluxinator", "Enter the Mono<Void>", "Hot Flux", "Streaming Las Vegas",
-					"Flux to the Future", "AEon Flux", "The Streaming", "Flux and Furious", "The Mighty Flux")
-					.map(title -> new Movie(title, randomGenre())));
-			movieRepository.save(flux).subscribe(System.out::println);
-		};
-	}
+    @Bean
+    CommandLineRunner demoData(MovieRepository movieRepository) {
+        return args -> {
+            movieRepository.deleteAll().block();
+            Flux<Movie> flux = Flux.fromStream(Stream.of("Y Tu Mono Tambien", "Flux Gordon", "Silence of the Lambdas", "FluxMan vs Superman",
+                    "The Fluxinator", "Enter the Mono<Void>", "Hot Flux", "Streaming Las Vegas",
+                    "Flux to the Future", "AEon Flux", "The Streaming", "Flux and Furious", "The Mighty Flux")
+                    .map(title -> new Movie(title, randomGenre())));
+            movieRepository.save(flux).subscribe(System.out::println);
+        };
+    }
 
-	private String randomGenre() {
-		String genres[] = "drama,romcom,documentary,horror,comedy".split(",");
-		return genres[new Random().nextInt(genres.length)];
-	}
+    private String randomGenre() {
+        String genres[] = "drama,romcom,documentary,horror,comedy".split(",");
+        return genres[new Random().nextInt(genres.length)];
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(FluxFlixServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FluxFlixServiceApplication.class, args);
+    }
 }
 
 @RestController
 @RequestMapping("/movies")
 class MovieRestController {
 
-	private final FluxFlixService fluxFlixService;
-	private final MovieRepository movieRepository;
+    private final FluxFlixService fluxFlixService;
+    private final MovieRepository movieRepository;
 
-	public MovieRestController(FluxFlixService fluxFlixService, MovieRepository movieRepository) {
-		this.fluxFlixService = fluxFlixService;
-		this.movieRepository = movieRepository;
-	}
+    public MovieRestController(FluxFlixService fluxFlixService, MovieRepository movieRepository) {
+        this.fluxFlixService = fluxFlixService;
+        this.movieRepository = movieRepository;
+    }
 
-	@GetMapping
-	public Flux<Movie> all() {
-		return this.movieRepository.findAll();
-	}
+    @GetMapping
+    public Flux<Movie> all() {
+        return this.movieRepository.findAll();
+    }
 
-	@GetMapping("/{id}/streams")
-	public Flux<ServerSentEvent<MovieStream>> streamsFor(@PathVariable String id) {
-		return this.movieRepository.findOne(id)
-				.flatMap(this.fluxFlixService::streamStreamsFor)
-				.map(m -> ServerSentEvent.builder(m).build());
-	}
+    @GetMapping(value = "/{id}/streams", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<MovieStream> streamsFor(@PathVariable String id) {
+        return this.movieRepository.findOne(id)
+                .flatMap(this.fluxFlixService::streamStreamsFor);
+    }
 
-	@GetMapping("/{id}")
-	public Mono<Movie> byId(@PathVariable String id) {
-		return this.movieRepository.findOne(id);
-	}
+    @GetMapping("/{id}")
+    public Mono<Movie> byId(@PathVariable String id) {
+        return this.movieRepository.findOne(id);
+    }
 }
 
 @Service
 class FluxFlixService {
 
-	private final MovieRepository movieRepository;
+    private final MovieRepository movieRepository;
 
-	public FluxFlixService(MovieRepository movieRepository) {
-		this.movieRepository = movieRepository;
-	}
+    public FluxFlixService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
 
-	public Flux<MovieStream> streamStreamsFor(Movie movie) {
-		Flux<Long> durationFlux = Flux.interval(Duration.ofSeconds(1));
-		Flux<MovieStream> streamFlux = Flux.fromStream(Stream.generate(() -> new MovieStream("starbuxman", movie, new Date())));
-		return Flux.zip(durationFlux, streamFlux).map(Tuple2::getT2);
-	}
+    public Flux<MovieStream> streamStreamsFor(Movie movie) {
+        Flux<MovieStream> streamFlux = Flux.fromStream(
+            Stream.generate(() -> new MovieStream(
+                Math.random() > .5 ? "starbuxman" : "mkheck", movie, new Date())));
+        Flux<Long> durationFlux = Flux.interval(Duration.ofSeconds(1));
+        return Flux.zip(streamFlux, durationFlux).map(Tuple2::getT1);
+    }
 
-	public Mono<Movie> findByTitle(String title) {
-		return this.movieRepository.findByTitle(title);
-	}
+    public Mono<Movie> findByTitle(String title) {
+        return this.movieRepository.findByTitle(title);
+    }
 
-	public Flux<Movie> findRelatedMovies(Movie m) {
-		return this.movieRepository.findByGenre(m.getGenre());
-	}
+    public Flux<Movie> findRelatedMovies(Movie m) {
+        return this.movieRepository.findByGenre(m.getGenre());
+    }
 }
 
 interface MovieRepository extends ReactiveMongoRepository<Movie, String> {
-	Mono<Movie> findByTitle(String title);
+    Mono<Movie> findByTitle(String title);
 
-	Flux<Movie> findByGenre(String genre);
+    Flux<Movie> findByGenre(String genre);
 }
 
 @Configuration
 @EnableReactiveMongoRepositories
 class ReactiveMongoConfiguration extends AbstractReactiveMongoConfiguration {
 
-	@Override
-	public com.mongodb.reactivestreams.client.MongoClient mongoClient() {
-		return MongoClients.create();
-	}
+    @Override
+    public com.mongodb.reactivestreams.client.MongoClient mongoClient() {
+        return MongoClients.create();
+    }
 
-	@Override
-	protected String getDatabaseName() {
-		return "movies";
-	}
+    @Override
+    protected String getDatabaseName() {
+        return "movies";
+    }
 }
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 class MovieStream {
-	private String user;
-	private Movie movie;
-	private Date when;
+    private String user;
+    private Movie movie;
+    private Date when;
 }
 
 @Data
@@ -146,12 +148,12 @@ class MovieStream {
 @AllArgsConstructor
 class Movie {
 
-	@Id
-	private String id;
-	private String genre, title;
+    @Id
+    private String id;
+    private String genre, title;
 
-	public Movie(String title, String genre) {
-		this.genre = genre;
-		this.title = title;
-	}
+    public Movie(String title, String genre) {
+        this.genre = genre;
+        this.title = title;
+    }
 }
