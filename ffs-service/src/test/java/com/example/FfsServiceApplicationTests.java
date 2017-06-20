493cc6440075613e1dc76dfaefcfa0e14efec345
Rob Winch <rwinch@users.noreply.github.com>
2017-06-20 11:04:38 -0500

    Demo Security WebTestClient support
    We can run our tests with an actual user by authenticating the request.
    However, this couples our logic to which users currently exist. This
    is not ideal when we are just wanting to test logic other than
    authentication (i.e. authorization).
    
    If we are not concerned with authentication for our test we can use
    withUser() to run as a specific user when running as WebTestClient in
    a mocked request (i.e. it won't work for full integration tests)

--- ffs-service/src/test/java/com/example/FfsServiceApplicationTests.java
+++ ffs-service/src/test/java/com/example/FfsServiceApplicationTests.java
@@ -1,16 +1,85 @@
 package com.example;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.ExchangeMutatorWebFilter;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.springframework.security.test.web.reactive.server.SecurityExchangeMutators.withUser;
+import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FfsServiceApplicationTests {
+	@Autowired
+	ApplicationContext context;
 
-	@Test
-	public void contextLoads() {
+	ExchangeMutatorWebFilter mutator = new ExchangeMutatorWebFilter();
+
+	WebTestClient client;
+
+	@Before
+	public void setup() {
+		client = WebTestClient
+				.bindToApplicationContext(context)
+				.webFilter(mutator)
+				.configureClient()
+				.baseUrl("http://localhost:8080/")
+				.build();
 	}
 
+	@Test
+	public void getMoviesWhenNotAuthenticatedThenIsUnauthorized() {
+		client
+				.get()
+				.uri("/movies/")
+				.exchange()
+				.expectStatus().isUnauthorized();
+	}
+
+	@Test
+	public void getMoviesWhenNotAdminThenIsForbidden() {
+		client
+				.filter(basicAuthentication("rob","password"))
+				.get()
+				.uri("/movies/")
+				.exchange()
+				.expectStatus().isEqualTo(HttpStatus.FORBIDDEN);
+	}
+
+	@Test
+	public void getMoviesWhenIsAdminThenIsOk() {
+		client
+				.filter(basicAuthentication("josh","password"))
+				.get()
+				.uri("/movies/")
+				.exchange()
+				.expectStatus().isOk();
+	}
+
+	@Test
+	public void getMoviesWhenMockNotAdminThenIsForbidden() {
+		client
+				.filter(mutator.perClient(withUser()))
+				.get()
+				.uri("/movies/")
+				.exchange()
+				.expectStatus().isEqualTo(HttpStatus.FORBIDDEN);
+	}
+
+	@Test
+	public void getMoviesWhenMockIsAdminThenIsOk() {
+		client
+				.filter(mutator.perClient(withUser().roles("ADMIN")))
+				.get()
+				.uri("/movies/")
+				.exchange()
+				.expectStatus().isOk();
+	}
 }
